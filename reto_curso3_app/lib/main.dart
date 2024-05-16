8ae5c9f66b2c767cb4cf6d4a33a20f8d4a96d26a
JuanMiranda27 <jcmiranda_27@hotmail.com>
2024-05-16 17:20:02 -0500

    Finaliza reto del capitulo 3

--- reto_curso3_app/lib/main.dart
+++ reto_curso3_app/lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    appBar: AppBar(title: const Text('Juan Carlos Miranda Aguilar 💙')),
-    body: const Center(
-        child: Text(
-            'Hola a todos, en mi tiempo libre disfruto de mi familia, vamos a pasear, al cine o simplemente vemos una pelicula en casa. Ahora estoy aprovechando este tiempo para aprender Flutter, que cada vez me parece mas interesante ')),
+    backgroundColor: Colors.blueAccent[100],
+    appBar: AppBar(title: const Text('Juan Carlos Miranda Aguilar ')),
+    body: const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+          child: Text(
+        'Hola a todos, en mi tiempo libre disfruto de mi familia👨‍👨‍👧‍👧, vamos a pasear🚗🏖☀, al cine🎞🎬🎥 o simplemente vemos una pelicula en casa 🏡📺🎬🎥📽. Ahora estoy aprovechando este tiempo para aprender Flutter💙, que cada vez me parece mas interesante ',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20),
+      )),
+    ),
   )));
 }
