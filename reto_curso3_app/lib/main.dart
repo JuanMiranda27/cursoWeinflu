96bcfa8049ff99f20cb223e105b5bad1367db07f
JuanMiranda27 <jcmiranda_27@hotmail.com>
2024-05-16 15:39:33 -0500

    agrega icono a nombre

--- reto_curso3_app/lib/main.dart
+++ reto_curso3_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    appBar: AppBar(title: const Text('Juan Carlos Miranda Aguilar')),
+    appBar: AppBar(title: const Text('Juan Carlos Miranda Aguilar 💙')),
     body: const Center(
         child: Text(
             'Hola a todos, en mi tiempo libre disfruto de mi familia, vamos a pasear, al cine o simplemente vemos una pelicula en casa. Ahora estoy aprovechando este tiempo para aprender Flutter, que cada vez me parece mas interesante ')),
